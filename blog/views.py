aec0c9b668862dc744f48468dbbddca6340d8b6e
Migheli <98918370+Migheli@users.noreply.github.com>
2022-05-08 06:31:22 +0300

    add popular posts counter

--- blog/views.py
+++ blog/views.py
@@ -29,7 +29,12 @@ def serialize_tag(tag):
 
 def index(request):
 
-    most_popular_posts = []  # TODO. Как это посчитать?
+    posts = Post.objects.all()
+
+    def get_likes_count(post):
+        return post.likes.count()
+    popular_posts = sorted(posts, key=get_likes_count)
+    most_popular_posts = list(popular_posts)[-5:]
 
     fresh_posts = Post.objects.order_by('published_at')
     most_fresh_posts = list(fresh_posts)[-5:]
