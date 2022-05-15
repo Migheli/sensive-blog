980a6ebe384063a420581e8b8e996957c852bcab
Migheli <98918370+Migheli@users.noreply.github.com>
2022-05-15 22:24:21 +0300

    Correct order of models

--- blog/models.py
+++ blog/models.py
@@ -36,38 +36,42 @@ class PostQuerySet(models.QuerySet):
 
 
 class Post(models.Model):
+
     title = models.CharField('Заголовок', max_length=200)
     text = models.TextField('Текст')
     slug = models.SlugField('Название в виде url', max_length=200)
     image = models.ImageField('Картинка')
     published_at = models.DateTimeField('Дата и время публикации')
-    objects = PostQuerySet.as_manager()
 
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
         verbose_name='Автор',
         limit_choices_to={'is_staff': True})
+
     likes = models.ManyToManyField(
         User,
         related_name='liked_posts',
         verbose_name='Кто лайкнул',
         blank=True)
+
     tags = models.ManyToManyField(
         'Tag',
         related_name='posts',
         verbose_name='Теги')
+
+    objects = PostQuerySet.as_manager()
+
+    class Meta:
+        ordering = ['-published_at']
+        verbose_name = 'пост'
+        verbose_name_plural = 'посты'
 
     def __str__(self):
         return self.title
 
     def get_absolute_url(self):
         return reverse('post_detail', args={'slug': self.slug})
-
-    class Meta:
-        ordering = ['-published_at']
-        verbose_name = 'пост'
-        verbose_name_plural = 'посты'
 
 
 class TagQuerySet(models.QuerySet):
@@ -79,42 +83,45 @@ class TagQuerySet(models.QuerySet):
 
 
 class Tag(models.Model):
+
     title = models.CharField('Тег', max_length=20, unique=True)
     objects = TagQuerySet.as_manager()
-
-    def __str__(self):
-        return self.title
-
-    def clean(self):
-        self.title = self.title.lower()
-
-    def get_absolute_url(self):
-        return reverse('tag_filter', args={'tag_title': self.slug})
 
     class Meta:
         ordering = ['title']
         verbose_name = 'тег'
         verbose_name_plural = 'теги'
 
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('tag_filter', args={'tag_title': self.slug})
+
+    def clean(self):
+        self.title = self.title.lower()
+
 
 class Comment(models.Model):
+
+    text = models.TextField('Текст комментария')
+    published_at = models.DateTimeField('Дата и время публикации')
+
     post = models.ForeignKey(
         'Post',
         on_delete=models.CASCADE,
         verbose_name='Пост, к которому написан',
         related_name='comments')
+
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
         verbose_name='Автор')
 
-    text = models.TextField('Текст комментария')
-    published_at = models.DateTimeField('Дата и время публикации')
-
-    def __str__(self):
-        return f'{self.author.username} under {self.post.title}'
-
     class Meta:
         ordering = ['published_at']
         verbose_name = 'комментарий'
         verbose_name_plural = 'комментарии'
+
+    def __str__(self):
+        return f'{self.author.username} under {self.post.title}'
